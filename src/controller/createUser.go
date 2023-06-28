7ed998d53f9af2523f5e555430116fc54da8829f
Emerson Franco <contato.emersonfranco@gmail.com>
2023-06-28 08:52:41 -0300

    Aula 5.2

--- src/controller/createUser.go
+++ src/controller/createUser.go
@@ -1,1 +1,11 @@
-package createUser
+package controller
+
+import (
+	"github.com/EmesDev/GoLang-CRUD/src/configuration/rest_err"
+	"github.com/gin-gonic/gin"
+)
+
+func CreateUser(c *gin.Context) {
+	err := *rest_err.NewBadRequestError("Você chamou a rota de forma errada!")
+	c.JSON(err.Code, err)
+}
